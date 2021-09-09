efd788d5d9189d6d4efb3f4bc2e69c2a76631dff
Kris Zhang <kriszhang122507@gmail.com>
2021-09-09 14:44:41 +1200

    commit

--- index.js
+++ index.js
@@ -32,4 +32,15 @@ Client.on('messageCreate', message => {
     }
 })
 
+Client.on('messageCreate', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ding'){
+        Client.commands.get('ding').execute(message, args);
+    }
+})
+
 Client.login(token)
